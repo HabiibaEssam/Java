8daa00ea8af8767b79515cfe13fb748d1d99e311
HabiibaEssam <habiibaessam11111@gmail.com>
2025-07-12 20:39:00 +0300

    Add files via upload

--- multiplicationTable.java
+++ multiplicationTable.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
-
 public class multiplicationTable {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number to print its multiplication table: ");
-        int num = input.nextInt();
-
         for (int i = 1; i <= 10; i++) {
-            System.out.println(num + " x " + i + " = " + (num * i));
+            System.out.println("Multiplication Table for " + i + ":");
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+
+            System.out.println(); 
         }
     }
 }
-
