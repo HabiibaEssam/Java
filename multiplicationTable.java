a1b59fb545db8b7dc6e62e60bab211bcd56c4536
HabiibaEssam <habiibaessam11111@gmail.com>
2025-07-12 20:35:57 +0300

    Add files via upload

---
+++ multiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to print its multiplication table: ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
+
